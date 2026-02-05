9eb64c61bc01b187301c91159a750db277aa3034
FMedard <fmedard@gmail.com>
2026-02-05 22:29:35 +0100

    Gestion du saut

--- scripts/animated_sprite_2d.gd
+++ scripts/animated_sprite_2d.gd
@@ -1,10 +1,18 @@
 extends AnimatedSprite2D
 
+var game_started = false
+
 func _ready():
 	animation_changed.connect(_on_animation_changed)
+	# Attend un court instant avant d'activer les sons
+	await get_tree().create_timer(0.1).timeout
+	game_started = true
 
 func _on_animation_changed():
+	if not game_started:
+		return
+		
 	if animation == "run":
 		$Course.play()
-	else:
-		$Course.stop()
+	elif animation == "jump":
+		$Saut.play()
